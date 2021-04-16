93d4d7df85d87466c53eda4d6ded79f7ae2b7111
BLONDIN Léo <l.blondin@digi-lapa033.local>
2021-04-16 11:17:45 +0200

    console log fix

--- src/js/World/Floor.js
+++ src/js/World/Floor.js
@@ -51,8 +51,6 @@ export default class Floor {
       }
   })
 
-    console.log(this.floor.children);
-
     this.floor.children[57].material.onBeforeCompile = shader => {
       shader.vertexShader = shader.vertexShader.replace(
         `#include <fog_pars_vertex>`,
